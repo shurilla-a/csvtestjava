31068fc937172bd22d864a76f36713212b73be11
shurilla-a <shurilla@gmail.com>
2018-03-27 13:43:31 +0300

    3

--- src/main/java/CsvReader.java
+++ src/main/java/CsvReader.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 //import java.util.regex.Pattern;
 //import java.util.regex.Matcher;
@@ -30,49 +31,54 @@ public class CsvReader {
 
 
              String[] nextRecord;
+             System.out.println("start read file1");
              while ((nextRecord = csvReader.readNext()) != null) {
                 // System.out.println("Телефон   " + nextRecord[3] + "  Статус  " + nextRecord[5]);
                  mobizon.add(nextRecord);
              }
-             // System.out.println(mobizon.size());
+
+             System.out.println(mobizon.size());
 
 
              String[] nextRecord1;
+             System.out.println("start read file2");
              while ((nextRecord1 = csvReader1.readNext()) != null) {
                  //System.out.println("Телефон " + nextRecord1[0] + " Статус " + nextRecord1[4]);
                  mobizonlk1.add(nextRecord1);
 
              }
+             System.out.println(mobizonlk1.size());
+
+             long progress = 0;
              for (String[] mobiz1 : mobizon) {
+                for (String[] mobiz2 : mobizonlk1) {
+                    System.out.format("\r[%d/%d] ", progress, mobizon.size());
+                     //if ((mobiz1[4] == mobiz2[4]) && (mobiz1[5] == mobiz2[5])) {
+                    if ((mobiz1[3].equals(mobiz2[0]) && (mobiz1[5].equals(mobiz2[4]))) ) {
+                        System.out.println("Дата " + mobiz1[0] + " ID " + mobiz1[1] + " Номер " + mobiz1[3] + " Статус " + mobiz1[5] + " Дата и Время " + mobiz1[6]);
+
+                        CSVWriter writer = new CSVWriter(new FileWriter(OUT));
+                        writer.writeNext(mobiz1);
+                        writer.close();
+                    }
+
+         //            CSVWriter writer = new CSVWriter(new FileWriter(OUT));
+           //          writer.writeNext(mobiz1);
+             //        writer.close();
+
+                    //                 System.out.println("Дата " + mobiz1[0] + " ID " + mobiz1[1] + " Номер " + mobiz1[3] + " Статус " + mobiz1[5] + " Дата и Время " + mobiz1[6]);
 
 
-             for (String[] mobiz2 : mobizonlk1) {
+  ///               CSVWriter writer = new CSVWriter(new FileWriter(OUT));
+     //            writer.writeNext(mobiz1);
+       //          writer.close();
+                 // !!!!!!!!!!!!!!!! Задать вопрос Леха почему вывод здесь  !!!!!!!!!!!!!!!!!
+                }
 
-             if ((mobiz1[3] == mobiz2[0] ) && (mobiz1[5] == mobiz2[4])) ;
-
-  //               System.out.println("Дата "+ mobiz1 [0] + " ID " + mobiz1[1] + " Номер " + mobiz1[3] + " Статус " + mobiz1[5] + " Дата и Время " + mobiz1[6]);
-
-
-//             System.out.println("Дата "+ mobiz1 [0] + " ID " + mobiz1[1] + " Номер " + mobiz1[3] + " Статус " + mobiz1[5] + " Дата и Время " + mobiz1[6]);
-
-                 //CSVWriter writer = new CSVWriter(new FileWriter(OUT));
-                 //writer.writeNext(mobiz1);
-                 //writer.close();
+                progress++;
              }
-         System.out.println("Дата "+ mobiz1 [0] + " ID " + mobiz1[1] + " Номер " + mobiz1[3] + " Статус " + mobiz1[5] + " Дата и Время " + mobiz1[6]);
-// !!!!!!!!!!!!!!!! Задать вопрос Леха почему вывод здесь  !!!!!!!!!!!!!!!!!
-             }
-
-  //         Arrays.equals(ne,nextRecord1);
-//System.out.println(Arrays.equals(nextRecord,nextRecord1));
-//System.out.println(nextRecord1);
          } catch (IOException e) {
              System.out.println(e.getMessage());
          }
-
+    }
 }
-        }
-
-
-
-
