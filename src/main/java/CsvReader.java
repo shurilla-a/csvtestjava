deecee18e93f2366f1062eb20fc002a653aa9592
shurilla-a <shurilla@gmail.com>
2018-03-30 14:30:50 +0300

    3

--- src/main/java/CsvReader.java
+++ src/main/java/CsvReader.java
@@ -1,31 +1,25 @@
 import com.opencsv.CSVReader;
 import com.opencsv.CSVWriter;
 import java.io.FileReader;
-//import java.io.Reader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
-//import java.util.regex.Pattern;
-//import java.util.regex.Matcher;
-//import java.nio.file.Files;
-//import java.nio.file.Paths;
 
 public class CsvReader {
 
     private static final String SAMPLE  = "//home//shurilla//mobizon.csv";//задаем константу пути к файлу
     private static final String SAMPLE1 = "//home//shurilla//mobizonlk2.csv";
     private static final String OUT = "//home//shurilla//out.csv";
-    //char csvSplitby = ';';
+
     public static void main (String [] agrs)
-//            throws IOException
+
     {
          try {
              ArrayList<String[]> mobizon = new ArrayList<>();
              ArrayList<String[]> mobizonlk1 = new ArrayList<>();
 
-             // Reader reader = Files.newBufferedReader(Paths.get(SAMPLE ));
+
              CSVReader csvReader = new CSVReader(new FileReader(SAMPLE));
              CSVReader csvReader1 = new CSVReader(new FileReader(SAMPLE1));
 
@@ -33,7 +27,7 @@ public class CsvReader {
              String[] nextRecord;
              System.out.println("start read file1");
              while ((nextRecord = csvReader.readNext()) != null) {
-                // System.out.println("Телефон   " + nextRecord[3] + "  Статус  " + nextRecord[5]);
+
                  mobizon.add(nextRecord);
              }
 
@@ -43,7 +37,7 @@ public class CsvReader {
              String[] nextRecord1;
              System.out.println("start read file2");
              while ((nextRecord1 = csvReader1.readNext()) != null) {
-                 //System.out.println("Телефон " + nextRecord1[0] + " Статус " + nextRecord1[4]);
+
                  mobizonlk1.add(nextRecord1);
 
              }
@@ -54,7 +48,7 @@ public class CsvReader {
                 for (String[] mobiz2 : mobizonlk1) {
                     System.out.format("\r[%d/%d] ", progress, mobizon.size());
                      //if ((mobiz1[4] == mobiz2[4]) && (mobiz1[5] == mobiz2[5])) {
-                    if ((mobiz1[3].equals(mobiz2[0]) && (mobiz1[5].equals(mobiz2[4]))) ) {
+                    if ((mobiz1[3].equals(mobiz2[0]) && (mobiz1[5].equals(mobiz2[4])) && (mobiz1[6].equals(mobiz2[1])) ) ) {
                         System.out.println("Дата " + mobiz1[0] + " ID " + mobiz1[1] + " Номер " + mobiz1[3] + " Статус " + mobiz1[5] + " Дата и Время " + mobiz1[6]);
 
                         CSVWriter writer = new CSVWriter(new FileWriter(OUT,true));
@@ -62,16 +56,6 @@ public class CsvReader {
                          writer.flush();
                     }
 
-//                     CSVWriter writer = new CSVWriter(new FileWriter(OUT));
-//                     writer.writeNext(mobiz1);
-//                     writer.close();
-
-                    //                 System.out.println("Дата " + mobiz1[0] + " ID " + mobiz1[1] + " Номер " + mobiz1[3] + " Статус " + mobiz1[5] + " Дата и Время " + mobiz1[6]);
-
-
-  ///               CSVWriter writer = new CSVWriter(new FileWriter(OUT));
-     //            writer.writeNext(mobiz1);
-       //          writer.close();
 
                 }
 
--- src/main/java/ReGexNew.java
+++ src/main/java/ReGexNew.java
@@ -6,17 +6,17 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.nio.file.*;
 public class ReGexNew {
-//
-//     private static final String SAMPLE = "//home//shurilla//mobizon.csv";
-//     private static final String SAMPLE2 = "//home//shurilla//mobizonlk1.csv";
-//
+
+     private static final String SAMPLE = "//home//shurilla//mobizon.csv";
+     private static final String SAMPLE2 = "//home//shurilla//mobizonlk1.csv";
+
      public static void main (String[] args) {
-          }
-      }
-//
-//         try {
-//
-//           ArrayList<String[]>mobizon = new ArrayList<>();
+
+
+
+         try {
+
+//          ArrayList<String[]>mobizon = new ArrayList<>();
 //           ArrayList<String[]>mobizonlk1 = new ArrayList<>();
 //
 //           CSVReader csvReader2 = new CSVReader(new FileReader(SAMPLE));
